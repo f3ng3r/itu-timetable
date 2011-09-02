3a09ec4f1752ce183b3ea86d75643d722a71bb49
Simon Henriksen <simonwh@gmail.com>
2011-09-03 01:44:46 +0200

    derp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItuTimetable::Application.config.secret_token = '63b64ed9746eca2e91498e1ce7e14257c346a2e3a28b3986ad45189e809e2ee4664951fd5a0ef775e3b1daa17d0a7ddd1b3c372ec02d74061a17c221a5df5c0a'
---
+++ test/functional/timetables_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TimetablesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
